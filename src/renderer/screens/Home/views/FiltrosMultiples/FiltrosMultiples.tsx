3bb796b3ce3451d14d9faf707d934f0c17e9b3b7
colmena29-rf <git.ricardof@colmena29.mx>
2024-06-25 12:07:39 -0600

    Filtro multiples page

--- src/renderer/screens/Home/views/FiltrosMultiples/FiltrosMultiples.tsx
+++ src/renderer/screens/Home/views/FiltrosMultiples/FiltrosMultiples.tsx
@@ -7,11 +7,20 @@ import { Button } from 'renderer/components'
 import CardBox from 'renderer/components/CardBox/CardBox'
 import MainTable from '../../components/MainTable'
 
+
+const HardCodeInput = (P: { text: string, noSelect?: boolean }) => {
+   return <Button variant='light' className='card-btn w-100 v-center-between flex-1'>
+      <p style={{ paddingRight: 40 }}>{P.text}</p>
+      {P.noSelect ? null : <FontAwesomeIcon icon={faChevronDown} />}
+   </Button>
+}
+
+
 export default function FiltrosMultiples() {
    const navigate = useNavigate()
 
    return (
-      <div>
+      <div id='filtros-multiples'>
          {/* Filtros */}
          <CardBox className='mb-4'>
 
@@ -26,10 +35,10 @@ export default function FiltrosMultiples() {
                </Button>
             </div>
 
-            <div className='wrapper-filter mt-4'>
+            <div className='wrapper-filter'>
                <div>
                   <p className='form-header'>Estado</p>
-                  <Button variant='light' className='card-btn v-center-between w-100'>
+                  <Button variant='light' className='card-btn v-center-between w-100 m-0'>
                      <p style={{ paddingRight: 40 }}>Estado</p>
                      <FontAwesomeIcon icon={faChevronDown} />
                   </Button>
@@ -37,50 +46,183 @@ export default function FiltrosMultiples() {
 
                <Row>
                   {/* Tipificación */}
-                  <Col>
+                  <Col xs={6}>
+                     <p className='form-header'>Tipificacion</p>
 
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Tipo' />
+                        <HardCodeInput text='Subtipo' />
+                     </Row>
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Particularidad' />
+                        <HardCodeInput text='Prioridad' />
+                     </Row>
+                  </Col>
+
+                  {/* Texto */}
+                  <Col xs={6}>
+                     <p className='form-header'>Texto</p>
+
+                     <Row className='d-flex gap-3'>
+                        <Form.Control
+                           as="textarea"
+                           className='flex-1 card-btn'
+                           placeholder="Hecho, relatos, comentarios"
+                           style={{ height: '80px' }}
+                        />
+                        <HardCodeInput text='Razon de cierre' />
+                     </Row>
+                  </Col>
+
+                  {/* Recursos */}
+                  <Col xs={6}>
+                     <p className='form-header'>Recursos</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Recursos asignados' />
+                        <HardCodeInput text='Recursos' />
+                     </Row>
+                  </Col>
+
+                  {/* Parte policial */}
+                  <Col xs={6}>
+                     <p className='form-header'>Parte policial</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Recursos' />
+                     </Row>
+                  </Col>
+
+                  {/* Fecha */}
+                  <Col xs={6}>
+                     <p className='form-header'>Fecha</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Desde' />
+                        <HardCodeInput text='Hasta' />
+                     </Row>
+                  </Col>
+
+                  {/* Entidades */}
+                  <Col xs={6}>
+                     <p className='form-header'>Entidades</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Agencia involucrada' />
+                        <HardCodeInput text='Centro involucrado' />
+                     </Row>
+                  </Col>
+
+                  {/* Usuario */}
+                  <Col xs={6}>
+                     <p className='form-header'>Usuario</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Usuario creador' />
+                        <HardCodeInput text='Terminal' noSelect />
+                     </Row>
+                  </Col>
+
+                  {/* Usuario (2nd col) */}
+                  <Col xs={6}>
+                     <p className='form-header' style={{ color: 'transparent' }}>Usuario</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Guardia/Turno' />
+                        <HardCodeInput text='Posicion' noSelect />
+                     </Row>
+                  </Col>
+
+                  {/* Agencia creadora */}
+                  <Col xs={6}>
+                     <p className='form-header'>Agencia creadora</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Agencia' />
+                     </Row>
+                  </Col>
+
+                  {/* Agencia receptora */}
+                  <Col xs={6}>
+                     <p className='form-header'>Agencia receptora</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Agencia' />
+                     </Row>
+                  </Col>
+
+                  {/* Telefono  */}
+                  <Col xs={6}>
+                     <p className='form-header'>Telefono </p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Telefono (libre)' noSelect />
+                        <HardCodeInput text='Motivo' />
+                     </Row>
+                  </Col>
+
+                  {/* Numero de carta */}
+                  <Col xs={6}>
+                     <p className='form-header'>Numero de carta</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Carta' noSelect />
+                     </Row>
+                  </Col>
+
+                  {/* Despacho | Aprehendidos*/}
+                  <Col xs={6}>
+                     <p className='form-header'>Despacho</p>
+
+                     <div className='mx-3 mb-2'>
+                        <Form.Check
+                           type={'checkbox'}
+                           id={`Solo de mi grupo de despacho`}
+                           label={`Solo de mi grupo de despacho`}
+                        />
+                     </div>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Grupo Despacho' />
+                        <HardCodeInput text='Recursos' />
+                     </Row>
+
+                     <p className='form-header'>Aprehendidos</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Aprehendidos' noSelect />
+                        <HardCodeInput text='Cantidad' noSelect />
+                     </Row>
+                  </Col>
+
+                  {/* Localizacion */}
+                  <Col xs={6}>
+                     <p className='form-header' style={{ marginBottom: 32 }}>Localizacion</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Municipio' />
+                        <HardCodeInput text='Localidad' />
+                     </Row>
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Localizacion' />
+                        <HardCodeInput text='Calle' noSelect />
+                     </Row>
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Altura' noSelect />
+                        <HardCodeInput text='Hito' noSelect />
+                     </Row>
+                  </Col>
+
+                  {/* Denunciante */}
+                  <Col xs={12}>
+                     <p className='form-header'>Denunciante</p>
+
+                     <Row className='d-flex gap-3'>
+                        <HardCodeInput text='Denunciante (libre)' noSelect />
+                        <HardCodeInput text='Documento' noSelect />
+                     </Row>
                   </Col>
                </Row>
-
-
-
-
-
-
-               <Form.Check
-                  type={'radio'}
-                  id={`default-radio`}
-                  label={`Ultimas dos horas`}
-               />
-               <Form.Check
-                  type={'radio'}
-                  id={`default-radio`}
-                  label={`Carta`}
-               />
-
-               <Button variant='light' className='card-btn'>
-                  <p>Numerico</p>
-               </Button>
-
-               <Button variant='light' className='card-btn v-center gap-2'>
-                  <FontAwesomeIcon icon={faCalendar} />
-                  <p>Desde</p>
-                  <FontAwesomeIcon icon={faChevronDown} />
-               </Button>
-
-               <Button variant='light' className='card-btn v-center gap-2'>
-                  <FontAwesomeIcon icon={faCalendar} />
-                  <p>Hasta</p>
-                  <FontAwesomeIcon icon={faChevronDown} />
-               </Button>
-
-               <Button variant='light' className='card-btn v-center gap-2'>
-                  <FontAwesomeIcon icon={faBookmark} />
-                  <p style={{ paddingRight: 40 }}>Estado</p>
-                  <FontAwesomeIcon icon={faChevronDown} />
-               </Button>
-
-               <Button style={{ width: '10rem' }} onClick={() => null}>Filtrar</Button>
             </div>
          </CardBox>
 
@@ -90,3 +232,4 @@ export default function FiltrosMultiples() {
       </div>
    )
 }
+
